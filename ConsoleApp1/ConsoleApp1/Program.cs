cf4dcd42626755b51079177e331b22190166dda0
Sohan Hasan <sohan.hasan9021@gmail.com>
2022-06-13 17:51:15 +0600

    updated

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            RunObservableCollectionCode();
+        }
+
+        private static void RunObservableCollectionCode()
+        {
+            ObservableCollection<string> names = new ObservableCollection<string>();
+            names.CollectionChanged += names_CollectionChanged;
+            names.Add("Adam");
+            names.Add("Eve");
+            names[0] = "Sohan";
+            names[0] = "Sohan";
+            names.Remove("Adam");
+            names.Add("John");
+            names.Add("Peter");
+
+            names.Clear();
+        }
+
+        static void names_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Debug.WriteLine("Change type: " + e.Action);
+            if (e.NewItems != null)
+            {
+                Debug.WriteLine("Items added: ");
+                foreach (var item in e.NewItems)
+                {
+                    Debug.WriteLine(item);
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                Debug.WriteLine("Items removed: ");
+                foreach (var item in e.OldItems)
+                {
+                    Debug.WriteLine(item);
+                }
+            }
+        }
+    }
+
+}
